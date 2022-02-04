2fbce3f47050578206bd2614d104b01ea014c53e
Doug Latornell <dlatornell@eoas.ubc.ca>
2022-02-04 13:34:25 -0800

    Add dask cluster mgmt to extract sub-command

--- reshapr/core/extract.py
+++ reshapr/core/extract.py
@@ -23,6 +23,7 @@ import time
 from pathlib import Path
 
 import arrow
+import dask.distributed
 import structlog
 import yaml
 
@@ -37,21 +38,99 @@ def extract(config_file):
                         Please see :ref:`ReshaprExtractYAMLFile` for details.
     :type config_file: :py:class:`pathlib.Path`
     """
-    t_start = time.time()
-    with config_file.open() as f:
-        config = yaml.safe_load(f)
-    log = logger.bind(config_file=os.fspath(config_file))
-    log.debug("read config")
+    config = _load_config(config_file)
+    dask_client = get_dask_client(config["dask cluster"])
+
     start_date = arrow.get(config["start date"])
     end_date = arrow.get(config["end date"])
-    log = log.bind(
+    log = logger.bind(
         start_date=start_date.format("YYYY-MM-DD"),
         end_date=end_date.format("YYYY-MM-DD"),
     )
 
+    t_start = time.time()
+
     t_total = time.time() - t_start
     log = log.bind(t_total=t_total)
     log.info(f"total time: {t_total:.3f}s")
+
+    dask_client.close()
+
+
+def _load_config(config_file):
+    """
+    :param config_file: File path and name of the YAML file to read processing configuration
+                        dictionary from.
+                        Please see :ref:`ReshaprExtractYAMLFile` for details.
+    :type config_file: :py:class:`pathlib.Path`
+    """
+    log = logger.bind(config_file=os.fspath(config_file))
+    with config_file.open() as f:
+        config = yaml.safe_load(f)
+    log.debug("loaded config")
+    return config
+
+
+## Functions below will probably eventually  be refactored into separate module(s)
+
+
+def get_dask_client(dask_config):
+    """Return a dask cluster client.
+
+    If :kbd:`dask_cluster` is a dask cluster configuration YAML file,
+    a :py:class:`dask.distributed.LocalCluster` is created using the parameters in the file,
+    and a client connected to that cluster is returned.
+    Otherwise,
+    :kbd:`dask_cluster` is assumed to be the IP address and port number of an existing cluster
+    in the form :kbd:`host_ip:port`,
+    and a client connected to that cluster is returned.
+
+    :param dask_config: File path and name of the YAML file to read the dask cluster configuration
+                        dictionary from,
+                        or a :kbd:`host_ip:port` string of an existing dask cluster fot connect to.
+                        Please see :ref:`ReshaprDaskClusterYAMLFile` for details.
+    :type dask_config: :py:class:`pathlib.Path` or str
+
+    :return: Dask cluster client.
+    :rtype: :py:class:`dask.distributed.Client`
+
+    :raises: :py:exc:`SystemExit` if a client cannot be created.
+    """
+    log = logger.bind(dask_config=dask_config)
+    try:
+        # Assume config is a YAML file path
+        with Path(dask_config).open("rt") as f:
+            cluster_config = yaml.safe_load(f)
+        log.debug("loaded dask cluster config")
+    except FileNotFoundError:
+        # Maybe config is a host_ip:port string
+        try:
+            # Connect to an existing cluster
+            client = dask.distributed.Client(dask_config)
+            log = log.bind(dashboard_link=client.dashboard_link)
+            log.info("dask cluster dashboard")
+            return client
+        except ValueError:
+            log.error(
+                "unrecognized dask cluster config; expected YAML file path or host_ip:port"
+            )
+            raise SystemExit(1)
+        except OSError:
+            log.error(
+                "requested dask cluster is not running or refused your connection"
+            )
+            raise SystemExit(1)
+    # Set up cluster described in YAML file
+    cluster = dask.distributed.LocalCluster(
+        name=cluster_config["name"],
+        n_workers=cluster_config["number of workers"],
+        threads_per_worker=cluster_config["threads per worker"],
+        processes=cluster_config["processes"],
+    )
+    client = dask.distributed.Client(cluster)
+    log = log.bind(dashboard_link=client.dashboard_link)
+    log.info("dask cluster dashboard")
+    return client
 
 
 # This stanza facilitates running the extract sub-command in a Python debugger
---
+++ tests/core/test_dask_cluster.py
@@ -0,0 +1,131 @@
+# Copyright 2022 – present, UBC EOAS MOAD Group and The University of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+"""Unit test for DaskCluster class.
+"""
+import textwrap
+
+import dask.distributed
+import pytest
+import structlog
+from structlog.testing import LogCapture
+
+from reshapr.core.extract import get_dask_client
+
+
+@pytest.fixture(name="log_output")
+def fixture_log_output():
+    return LogCapture()
+
+
+@pytest.fixture(autouse=True)
+def fixture_configure_structlog(log_output):
+    structlog.configure(processors=[log_output])
+
+
+@pytest.mark.filterwarnings("ignore:There is no current event loop:DeprecationWarning")
+class TestGetClient:
+    """Unit tests for DaskCluster.get_client() method."""
+
+    @pytest.mark.parametrize(
+        "dask_config",
+        (
+            "nonexistent.yaml",
+            "localhost",
+            "127.0.0.1",
+        ),
+    )
+    def test_bad_config(self, dask_config, log_output):
+        with pytest.raises(SystemExit) as exc_info:
+            get_dask_client(dask_config)
+
+        assert exc_info.value.code == 1
+        assert log_output.entries[0]["log_level"] == "error"
+        assert log_output.entries[0]["dask_config"] == dask_config
+        assert (
+            log_output.entries[0]["event"]
+            == "unrecognized dask cluster config; expected YAML file path or host_ip:port"
+        )
+
+    @pytest.mark.parametrize(
+        "dask_config",
+        (
+            "localhost:4343",
+            "127.0.0.1:4343",
+        ),
+    )
+    def test_no_cluster(self, dask_config, log_output, monkeypatch):
+        class MockClient:
+            def __init__(self, dask_config):
+                raise OSError
+
+        monkeypatch.setattr(dask.distributed, "Client", MockClient)
+
+        with pytest.raises(SystemExit) as exc_info:
+            get_dask_client(dask_config)
+
+        assert exc_info.value.code == 1
+        assert log_output.entries[0]["log_level"] == "error"
+        assert log_output.entries[0]["dask_config"] == dask_config
+        assert (
+            log_output.entries[0]["event"]
+            == "requested dask cluster is not running or refused your connection"
+        )
+
+    def test_connect_to_cluster(self, log_output, tmp_path):
+        cluster = dask.distributed.LocalCluster(
+            name="test dask cluster",
+            n_workers=1,
+            threads_per_worker=1,
+            processes=True,
+        )
+        dask_config = cluster.scheduler_address
+
+        client = get_dask_client(dask_config)
+        dashboard_link = client.dashboard_link
+        client.close()
+
+        assert log_output.entries[0]["log_level"] == "info"
+        assert log_output.entries[0]["dask_config"] == dask_config
+        assert log_output.entries[0]["dashboard_link"] == dashboard_link
+        assert log_output.entries[0]["event"] == "dask cluster dashboard"
+
+    def test_launch_cluster(self, log_output, tmp_path):
+        dask_config = tmp_path / "test_cluster.yaml"
+        dask_config.write_text(
+            textwrap.dedent(
+                """\
+                name: test dask cluster
+                processes: True
+                number of workers: 1
+                threads per worker: 1
+                """
+            )
+        )
+
+        client = get_dask_client(dask_config)
+        dashboard_link = client.dashboard_link
+        client.close()
+
+        assert log_output.entries[0]["log_level"] == "debug"
+        assert log_output.entries[0]["dask_config"] == dask_config
+        assert log_output.entries[0]["event"] == "loaded dask cluster config"
+
+        assert log_output.entries[1]["log_level"] == "info"
+        assert log_output.entries[1]["dask_config"] == dask_config
+        assert log_output.entries[1]["dashboard_link"] == dashboard_link
+        assert log_output.entries[1]["event"] == "dask cluster dashboard"
