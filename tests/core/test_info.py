23225ad5dcd1f60228578b8799e782103a037e72
Doug Latornell <dlatornell@eoas.ubc.ca>
2022-06-24 10:04:48 -0700

    Add in-pkg model profiles to info output

--- tests/core/test_info.py
+++ tests/core/test_info.py
@@ -25,7 +25,11 @@ from reshapr.core import info
 
 
 class TestInfo:
-    """Unit tests for core.info.info() function."""
+    """Unit tests for core.info.info() function.
+
+    NOTE: These tests are a bit brittle because they rely on hard-coded line numbers in the
+    captured stdout.
+    """
 
     @pytest.mark.parametrize(
         "pkg, line", (("reshapr", 0), ("xarray", 1), ("dask", 2), ("netcdf4", 3))
@@ -42,6 +46,16 @@ class TestInfo:
         stdout_lines = capsys.readouterr().out.splitlines()
         assert stdout_lines[6] == "  salish_cluster.yaml"
 
+    def test_model_profiles(self, capsys):
+        info.info()
+
+        stdout_lines = capsys.readouterr().out.splitlines()
+        assert stdout_lines[9] == "  SalishSeaCast-201905.yaml"
+        assert stdout_lines[10] == "  SalishSeaCast-201812.yaml"
+        assert stdout_lines[11] == "  HRDPS-2.5km-GEMLAM-22sep11onward.yaml"
+        assert stdout_lines[12] == "  HRDPS-2.5km-GEMLAM-pre22sep11.yaml"
+        assert stdout_lines[13] == "  HRDPS-2.5km-operational.yaml"
+
 
 class TestGetClusterConfigs:
     """Unit test for core.info._get_cluster_configs() function."""
@@ -51,3 +65,19 @@ class TestGetClusterConfigs:
 
         expected = ["salish_cluster.yaml"]
         assert cluster_configs == expected
+
+
+class TestGetModelProfiles:
+    """Unit test for core.info._get_model_profiles() function."""
+
+    def test_get_model_profiles(self):
+        model_profiles = info._get_model_profiles()
+
+        expected = [
+            "SalishSeaCast-201905.yaml",
+            "SalishSeaCast-201812.yaml",
+            "HRDPS-2.5km-GEMLAM-22sep11onward.yaml",
+            "HRDPS-2.5km-GEMLAM-pre22sep11.yaml",
+            "HRDPS-2.5km-operational.yaml",
+        ]
+        assert model_profiles == expected
